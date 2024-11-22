087f8ec56a1142a259e08f604d0fdbaec60ac2df
Victoria Ternovaya <victoria.ternovaya@gmail.com>
2024-11-22 23:13:45 +0300

    fix: burger animation

--- christmas-shop/scripts/main.js
+++ christmas-shop/scripts/main.js
@@ -1,34 +1,33 @@
 
 const menu = document.querySelector('.menu__list');
-const menuBtn = document.querySelector('.burger');
-const menuLine = document.querySelector('.burger__line')
+const burgerBtn = document.querySelector('.burger');
+const burgerLine = document.querySelector('.burger__line')
 const body = document.body;
 
-if (menu && menuBtn) {
-	menuBtn.addEventListener('click', e => {
+if (menu && burgerBtn) {
+	burgerBtn.addEventListener('click', e => {
 		menu.classList.toggle('menu__list--active');
-		menuBtn.classList.toggle('burger--active');
-        menuLine.classList.toggle('burger__line--active')
+		burgerBtn.classList.toggle('burger--active');
+        burgerLine.classList.toggle('burger__line--active')
 		body.classList.toggle('noscroll');
 	})
 
 	menu.addEventListener('click', e => {
-		if (e.target.classList.contains('menu__item-link')) {
+		if (e.target.classList.contains('menu__item')) {
 			menu.classList.remove('menu__list--active');
-			menuBtn.classList.remove('burger--active');
-			menuLine.classList.remove('burger__line--active');
+			burgerBtn.classList.remove('burger--active');
+			burgerLine.classList.remove('burger__line--active');
 			body.classList.remove('noscroll');
 		}
 	})
 
-	menu.querySelectorAll('.menu__item-link').forEach(link => {
+	menu.querySelectorAll('.menu__item').forEach(link => {
 		link.addEventListener('click', (event) => {
 			menu.classList.remove('menu__list--active');
-			menuBtn.classList.remove('burger--active');
-            menuLine.classList.remove('burger__line--active');
+			burgerBtn.classList.remove('burger--active');
+            burgerLine.classList.remove('burger__line--active');
 			body.classList.remove('noscroll');
             event._isClicked = true;
-            console.log("Клик по ссылке");
 		})
 	})
 };
@@ -38,13 +37,13 @@ body.addEventListener('click', (event) => {
     if( 
      event._isClicked === true ||
      event.target.classList.contains('menu__list') == true ||
-     event.target.classList.contains('menu__item-link') == true  ||
+     event.target.classList.contains('menu__item') == true  ||
      event.target.classList.contains('burger') == true ||
      event.target.classList.contains('burger__line') == true
     ) return;
 
    menu.classList.remove('menu__list--active');
-   menuBtn.classList.remove('burger--active');
-   menuLine.classList.remove('burger__line--active');
+   burgerBtn.classList.remove('burger--active');
+   burgerLine.classList.remove('burger__line--active');
    body.classList.remove('noscroll');
 });

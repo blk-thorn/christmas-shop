ffe77134405baf4ccfa4456ca3a0689f2a3e02df
Victoria Ternovaya <victoria.ternovaya@gmail.com>
2024-11-25 20:29:30 +0300

    feat: add modal window and dynamic cards

--- christmas-shop/scripts/main.js
+++ christmas-shop/scripts/main.js
@@ -51,8 +51,17 @@ body.addEventListener('click', (event) => {
 });
 
 
+// const scrollTopBtn = document.querySelector(".scroll-top")
+// window.addEventListener("scroll", trackScroll);
 
 
-
-
+// function trackScroll() {
+// 	const offset = window.scrollY;
+// 	const windowHeight = document.documentElement.clientHeight;
+// 	if(offset > windowHeight) {
+// 		scrollTopBtn.classList.add("scroll-top--active");
+// 	} else {
+// 		scrollTopBtn.classList.remove("scroll-top--active");
+// 	}
+// }
 

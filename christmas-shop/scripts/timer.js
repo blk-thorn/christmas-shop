4ade5dff6595766b128b4af4b31471af3f555ab6
Victoria Ternovaya <victoria.ternovaya@gmail.com>
2024-12-04 19:08:25 +0300

    fix: timer and scrollTop

--- christmas-shop/scripts/timer.js
+++ christmas-shop/scripts/timer.js
@@ -3,23 +3,18 @@ const hours = document.querySelector('.timer__hours');
 const minutes = document.querySelector('.timer__minutes');
 const seconds = document.querySelector('.timer__seconds');
 
-
 const currentYear = new Date().getUTCFullYear();
-const nextYear = new Date(Date.UTC(currentYear + 1, 0, 1, 0, 0, 0)); // Январь -  0
+const nextYear = Date.UTC(currentYear + 1, 0, 1, 0, 0, 0); // 1 января(0)
 
 function updateTimerCount() {
-    const currentTime = new Date();
-    const currentUTCTime = new Date(currentTime.getUTCFullYear(), currentTime.getUTCMonth(), currentTime.getUTCDate(),
-                                     currentTime.getUTCHours(), currentTime.getUTCMinutes(), currentTime.getUTCSeconds());
-    
-    const difference = nextYear - currentUTCTime;
+    const currentTime = Date.now();
+    const difference = nextYear - currentTime;
 
     const daysLeft = Math.floor(difference / 1000 / 60 / 60 / 24);
     const hoursLeft = Math.floor(difference / 1000 / 60 / 60) % 24;
     const minutesLeft = Math.floor(difference / 1000 / 60) % 60;
     const secondsLeft = Math.floor(difference / 1000) % 60;
-    
-    // console.log(daysLeft, hoursLeft, minutesLeft, secondsLeft);
+
     days.innerText = daysLeft;
     hours.innerText = hoursLeft;
     minutes.innerText = minutesLeft;
